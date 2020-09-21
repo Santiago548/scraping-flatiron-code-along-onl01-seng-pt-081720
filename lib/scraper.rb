0ded26d616d8cb528ced1be95eb5bdc3abc23ddf
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-21 20:24:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,4 +17,17 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.descrtiption = post.css("p").text
+    end
+  end
 end
