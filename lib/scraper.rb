7034f5355140983c83854f2f68f675dae850938b
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-21 20:30:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,4 +30,18 @@ class Scraper
       course.descrtiption = post.css("p").text
     end
   end
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "Schedule: #{course.schedule}"
+        puts "Description: #{course.description}"
+      end
+    end
+  end
+  
 end
+
+Scraper.new.print_courses
